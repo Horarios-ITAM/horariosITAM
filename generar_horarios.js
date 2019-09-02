19830cc24ddc3bb845ac6d3e9308334489ce4f2b
Emilio Cantu <emiliocantu.500@gmail.com>
2019-09-01 19:10:59 -0500

    Update generar_horarios.js

--- generar_horarios.js
+++ generar_horarios.js
@@ -280,7 +280,7 @@ function print_horario_html(horario,mobile=false){
     //console.log("print_horario_html",h)
     var out="";
     if(mobile){
-        var out='<table style="border-collapse: collapse;border: 1px solid black;font-size:7px;">';
+        var out='<table style="border-collapse: collapse;border: 1px solid black;font-size:10px;">';
 
     }else{
         var out='<table width="580" style="border-collapse: collapse;border: 1px solid black;font-size:10px;">';
