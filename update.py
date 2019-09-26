0b2815d50425197b67ce1f7bbf566bf436986c10
Emilio Cantu <emiliocantu.500@gmail.com>
2019-09-26 16:27:29 -0500

    .

--- update.py
+++ update.py
@@ -71,6 +71,30 @@ def levenshtein_ratio(a,b):
         except:edits_away+=1
     ratio=((len(a)+len(b)) - edits_away) / (len(a)+len(b))
     return ratio
+def replace_latin_chars(str):
+    translate = {"Á": "A", "É": "E", "Í": "I", "Ó": "O", "Ú": "U", "Ñ": "N", "Ü": "U","á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u", "ñ": "n", "ü": "u"}
+    for original,replacement in translate.items():
+        str.replace(original,replacement)
+    return str
+def fix_url(str):
+    nombre = str.strip();
+    nombre = replace_latin_chars(nombre);
+    nombre = nombre.replace("/[^a-zA-Z0-9\s]/", "");
+    nombre = nombre.replace("(", "");
+    nombre = nombre.replace(")", "");
+    nombre = nombre.replace(" -", "-");
+    nombre = nombre.replace("- ", "-");
+    nombre = nombre.replace("\n", "");
+    nombre = nombre.replace("\r", "");
+    nombre = nombre.replace("  ", " ");
+    nombre = nombre.replace("\\", "");
+    nombre = nombre.replace(" ", "-");
+    return nombre;
+def profesor_url(nombre, apellido, id):
+  nombre = fix_url(nombre);
+  apellido = fix_url(apellido);
+  #print("prof url ",nombre,apellido,id)
+  return "/profesores/"+nombre+"-"+apellido+"_"+id;
 def match(lista_profesITAM,misProfes):
     matched={}
     count=0
@@ -111,6 +135,7 @@ def scrapeMisProfes():
     for p in data:
         stripped[p['n']+' '+p['a']]=[p['c'],p['m']]
     return stripped
+
 def getRatings():
     todos_profesores=list(set([grupo['profesor']for clase in clases for grupo in clase['grupos'] if len(grupo['profesor'].strip())>1]))
     matched=match(todos_profesores,scrapeMisProfes())
@@ -118,12 +143,56 @@ def getRatings():
     print("\tlen todos_profesores: ",len(todos_profesores))
     print("\tlen matched: ",len(matched))
     return matched
-
-
+def matchLinks(lista_profesITAM,misProfes):
+    links={}
+    base_url= "https://www.misprofesores.com"
+    count=0
+    already=0
+    didnt=0
+    for profe in lista_profesITAM:
+        found=False
+        for miprofe in misProfes.keys():
+            if levenshtein_ratio(profe,miprofe)>0.90:
+                found=True
+                id=misProfes[miprofe]
+                #print(count,levenshtein_ratio(profe,miprofe),"profe:",profe,"miprofe:",miprofe,rating)
+                count+=1
+                #print(miprofe.split("*"),len(miprofe.split("*")))
+                nombre,apellido=miprofe.split("*")
+                links[profe]=base_url+profesor_url(nombre,apellido,id)
+        if not found:didnt+=1
+    return links
+def scrapeMisProfesLinks():
+    print("Scrapping links MisProfes.com ...")
+    url='https://www.misprofesores.com/escuelas/ITAM-Instituto-Tecnologico-Autonomo-de-Mexico_1003'
+    fp = urllib.request.urlopen(url)
+    mybytes = fp.read()
+    mystr = mybytes.decode("utf8")
+    fp.close()
+    b=mystr.index("dataSet")
+    data=mystr[mystr.index("[",b):mystr.index(";",b)]
+    data=json.loads(data)
+    stripped={}
+    for p in data:
+        stripped[p['n']+'*'+p['a']]=p['i']
+    return stripped
+def getLinks():
+    todos_profesores=list(set([grupo['profesor']for clase in clases for grupo in clase['grupos'] if len(grupo['profesor'].strip())>1]))
+    matched=matchLinks(todos_profesores,scrapeMisProfesLinks())
+    print("Matching data:")
+    print("\tlen todos_profesores: ",len(todos_profesores))
+    print("\tlen matched: ",len(matched))
+    return matched
+"""if __name__ == '__main__':
+    with open("clases.json","r",encoding="utf8")as f:
+        clases=json.loads(f.read())
+    print(getLinks())
+"""
 clases=scrapeHorariosITAM()
 print("Loaded {} clases!".format(len(clases)))
 lista_de_todas_clases=[clase['nombre'] for clase in clases]
 ratings=getRatings()
+links=getLinks()
 with open("generar_horarios.js","r",encoding="utf8") as f:
     old=f.read()
 essential=old[:old.index("/**DATA*/")]
@@ -135,5 +204,6 @@ with open("generar_horarios.js","w+",encoding="utf8") as f:
     f.write("\nvar lista_de_todas_clases="+json.dumps(lista_de_todas_clases))
     f.write("\nvar ratings="+json.dumps(ratings))
     f.write("\nvar clases="+json.dumps(clases))
+    f.write("\nvar links="+json.dumps(links))
 print("Updated!")
 input("Press enter to exit")
