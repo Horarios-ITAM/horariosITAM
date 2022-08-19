264ae2e576c278166c7d63a2237fa0d0bd9c171a
Emilio Cantu <emiliocantu.500@gmail.com>
2022-08-19 18:39:55 -0500

    server bash

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-python3 update/update.py && git add . && git commit -m "auto update" && git push
+# Solo correr en el servidor.
+# Script que corre el cron job para scrappear y hacer push a github.
+# Siempre hacemos pull y aceptamos todos los cambios entrantes.
+
+ git pull & git checkout --theirs . & python3 update/update.py && git add . && git commit -m "auto update" && git push
