e6da45c6ff20579a8486d7aee55d32bba18e9851
Emilio Cantu <emiliocantu.500@gmail.com>
2022-08-20 09:33:18 -0500

    bash file test

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@
 # Script que corre el cron job para scrappear y hacer push a github.
 # Siempre hacemos pull y aceptamos todos los cambios entrantes.
 
- git pull & git checkout --theirs . & python3 update/update.py && git add . && git commit -m "auto update" && git push
+ cd /home/horariosITAM/ & git pull & git checkout --theirs . & python3 update/update.py && git add . && git commit -m "auto update" && git push
