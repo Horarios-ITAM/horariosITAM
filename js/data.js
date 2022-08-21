5e7d93f14c7756574f7fe05a1b15cc2ad478a3aa
Emilio Cantu <emiliocantu.500@gmail.com>
2022-08-21 15:17:54 -0500

    auto update

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-let actualizado='1661008262067.6458';
+let actualizado='1661112842774.7412';
 let periodo='OTOÑO 2022 LICENCIATURA';
 let sGrace='2125';
 let dropDownUrl='https://serviciosweb.itam.mx/EDSUP/BWZKSENP.P_Horarios1?s=2125';
