9bedd7b445724dca36664aee79a1c7e15c2d612b
Emilio Cantu <emiliocantu.500@gmail.com>
2023-01-02 08:10:40 -0600

    argumento

--- shell/cacheBoletines.sh
+++ shell/cacheBoletines.sh
@@ -4,6 +4,8 @@
 # Script que corre el cron job para scrappear y hacer push a github.
 # Siempre hacemos pull y aceptamos todos los cambios entrantes.
 
+# NOTA: $1 = 'bruta' (sin comillas): se usa fuerza bruta para buscar boletines.
+
 cd /home/horariosITAM;
 
-git pull && git checkout --theirs . && python3 update/cacheCalendarios.py && git add . && git commit -m "auto update" && git push
+git pull && git checkout --theirs . && python3 update/cacheBoletines.py $1 && git add . && git commit -m "auto update" && git push
