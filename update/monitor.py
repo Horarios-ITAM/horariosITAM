357a9058d3189b9b73232e8163db45970ba8d4f1
Emilio Cantú <emiliocantu.500@gmail.com>
2024-05-15 11:53:30 -0600

    monitor init

--- update/monitor.py
+++ update/monitor.py
@@ -4,18 +4,21 @@
 import requests, argparse, time
 from bs4 import BeautifulSoup
 
-def notifica(msg, url, channel):
+def notifica(msg, url):
+    if not CHANNEL:
+        print('No hay canal de notificaciones!')
+        exit(1)
 
     print(f'Notificando {msg}')
     requests.post(
-        f"https://ntfy.sh/{channel}",
+        f"https://ntfy.sh/{CHANNEL}",
         data = msg.encode('utf-8'),
         headers = {
             "Click": url,
             "Tags": "warning,horariositam"
     })
 
-def req(url):
+def req(url, exit_on_error = True):
     r = None
     try:
         r = requests.get(url)
@@ -23,22 +26,24 @@ def req(url):
             raise Exception(f'Codigo de respuesta {r.status_code} para {url}')
     except Exception as e:
         print(f'Error al acceder a {url}: {e}')
-        notifica(f'Sitio inaccesible', url)
-        exit(1)
+        notifica(f'Sitio inaccesible: {url}', url)
+        if exit_on_error: exit(1)
     return r
 
-def checa_actualizado_hace(url, channel, dias_max = 2):
+def checa_actualizado_hace(url, dias_max = 2):
 
-    r = req(url)
+    r = req(url, exit_on_error = False)
+    if r is None: return
+
     pag = url.split('/')[-1]
 
     # Debe ser primera linea en formato "var actualizado = 1620000000000;"
     actualizado = r.text.split(';')[0].split('=')[1].replace('"', '').strip()
-    dias_desde = (time.time() - float(actualizado) / 1000) / 60 / 60 #/ 24
+    dias_desde = (time.time() - float(actualizado) / 1000) / 60 / 60 / 24
 
     if dias_desde > dias_max:
         print(f'{pag} actualizados hace {dias_desde:.2f} dias')
-        notifica(f'{pag} actualizados hace {dias_desde:.2f} dias', url, channel)
+        notifica(f'{pag} actualizados hace {dias_desde:.2f} dias', url)
 
 
 if __name__ == '__main__':
@@ -49,27 +54,26 @@ if __name__ == '__main__':
     args = argparser.parse_args()
 
     URL_BASE = args.url
-    channel = args.channel
+    global CHANNEL
+    CHANNEL = args.channel
 
 
     # Checamos que el sitio este arriba
     req(URL_BASE)
 
     # Que datos (index y profesores) esten actualizados (no mas de 2 dias)
-    checa_actualizado_hace(URL_BASE + '/js/datos/datos_index.js', channel)
-    checa_actualizado_hace(URL_BASE + '/js/datos/datos_profesores.js', channel)
+    checa_actualizado_hace(URL_BASE + '/js/datos/datos_index.js')
+    checa_actualizado_hace(URL_BASE + '/js/datos/datos_profesores.js')
 
     # Que tengamos ligas a calendarios
-    r = req(URL_BASE + '/calendarios.html')
-    b = BeautifulSoup(r.text, "html.parser")
-    if not b.find_all("a", {"class": "linkCalendario"}):
-        print('No hay ligas a calendarios')
-        notifica('No hay ligas a calendarios', URL_BASE + '/calendarios.html', channel)
+    r = req(URL_BASE + '/calendarios.html', exit_on_error = False)
+    if r is not None:
+        b = BeautifulSoup(r.text, "html.parser")
+        if not b.find_all("a", {"class": "linkCalendario"}):
+            print('No hay ligas a calendarios')
+            notifica('No hay ligas a calendarios', URL_BASE + '/calendarios.html')
 
-    # Y que funcionen
-    for hit in b.find_all("a", {"class": "linkCalendario"}):
-        req(URL_BASE + '/' + hit['href'])
-
-
-
-
+        # Y que funcionen
+        for hit in b.find_all("a", {"class": "linkCalendario"}):
+            print('Checando', hit['href'])
+            req(URL_BASE + '/' + hit['href'], exit_on_error = False)
