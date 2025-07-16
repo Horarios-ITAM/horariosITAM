cbcd9ae34f6fb8b46cd01bdab493af832afa8360
Emilio Cantú <emiliocantu.500@gmail.com>
2025-07-15 22:23:45 -0600

    clean up

--- update/misProfesScrapper.py
+++ update/misProfesScrapper.py
@@ -1,4 +1,8 @@
-import utils, json
+import requests
+import utils
+import json
+import re
+from urllib.parse import urljoin
 
 
 class MisProfesScrapper:
@@ -6,18 +10,20 @@ class MisProfesScrapper:
     Clase que maneja el scrapeo de profesores del ITAM de MisProfes.com.
     """
 
-    def __init__(self, url: str):
+    def __init__(self, url: str, url_profes: str = "https://www.misprofesores.com/profesores/"):
         """
-        Constructor.
-
         Parametros:
         ----------
         url: str.
             URL de la página de MisProfes.com designada al ITAM.
         """
         self.url = url
+        self.url_profes = url_profes
 
-    def _fixStr(self, s):
+        # self.data = {}
+
+    @staticmethod
+    def arreglaNombre(s):
         """
         Arregla strs de nombres y apellidos de profesores
         """
@@ -38,42 +44,59 @@ class MisProfesScrapper:
             s = s.replace(old, new)
         return s
 
-    def _urlProfe(self, nombre, apellido, id):
-        return (
-            "https://www.misprofesores.com/profesores/"
-            + self._fixStr(nombre)
-            + "-"
-            + self._fixStr(apellido)
-            + "_"
-            + id
-        )
+    def urlProfe(self, nombre, apellido, id):
+        """
+        Construye la URL del perfil del profesor en MisProfes.com.
+        """
+        nombre_limpio = self.arreglaNombre(nombre)
+        apellido_limpio = self.arreglaNombre(apellido)
+        
+        path = f"{nombre_limpio}-{apellido_limpio}_{id}"
+        return urljoin(self.url_profes, path)
+    
+    @staticmethod
+    def pruebaUrlProfe(url):
+        """
+        Prueba si la URL del profesor es valida.
+        """
+        try:
+            response = requests.get(url, verify=False)
+            return response.status_code == 200
+        except requests.RequestException:
+            return False
 
     def scrap(self):
+
         print("Scrappeando MisProfes ...")
         html = utils.getHTML(self.url)
-        start = html.index("var dataSet = ") + len("var dataSet = ")
-        tempData = json.loads(html[start : html.index("];", start) + 1])
-        profesores = {
+
+        # Extrae como JSON la variable `dataSet` del HTML
+        match = re.search(r"var\s+dataSet\s*=\s*(\[[\s\S]*?\]);", html)
+        if not match:
+            raise ValueError("No se pudo encontrar 'dataSet' en el HTML.")
+
+        # Lista de objectos con id (i), nombre (n), apellido (a), departamento (d), calif (c) y no. de evaluaciones (m)
+        dataSet = json.loads(match.group(1))
+
+        self.data = {
             p["n"] + " " + p["a"]: {
                 "c": p["c"],
                 "m": p["m"],
                 "i": p["i"],
                 "misProfesNom": p["n"] + " " + p["a"],
-                "link": self._urlProfe(p["n"], p["a"], p["i"]),
+                "link": self.urlProfe(p["n"], p["a"], p["i"]),
             }
-            for p in tempData
+            for p in dataSet
         }
-        print("Se scrappearon {} profesores de MisProfes!".format(len(profesores)))
-        self.data = profesores
+        print(f"Se scrappearon {len(self.data)} profesores de MisProfes!")
+
 
     def match(self, profesores, matchRate):
         # Match with levenshteinSimilarity
         matched = {}
         for m in self.data.keys():
             for p in profesores:
-                if (
-                    utils.levenshtein_ratio(p, m) > matchRate
-                ):  # utils.levenshteinSimilarity(p,m)>matchRate:
+                if utils.levenshtein_ratio(p, m) > matchRate:
                     matched[p] = self.data[m]
         # Format
         links = {p: d["link"] for p, d in matched.items()}
@@ -94,3 +117,9 @@ class MisProfesScrapper:
             )
         )
         return formatted
+
+
+if __name__ == "__main__":
+    url = "https://www.misprofesores.com/escuelas/ITAM-Instituto-Tecnologico-Autonomo-de-Mexico_1003"
+    scrapper = MisProfesScrapper(url)
+    scrapper.scrap()
